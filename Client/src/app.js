35e220427d2c4c85edcd2b950c591ad44e6dc3e7
anadi45 <ranjeetbaraik15@gmail.com>
2022-12-26 15:23:58 +0530

    exp date page

--- Client/src/app.js
+++ Client/src/app.js
@@ -9,9 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 
 
 function nameGenerator(prod_name) {
-    const date = (new Date()).toISOString();
-    let name = prod_name + "-" + date;
-    name = name.replace(":", "");
+    const dateObj = new Date();
+    let date = dateObj.getUTCDate();
+    let month = dateObj.getUTCMonth() + 1;
+    let year = dateObj.getUTCFullYear();
+    let name = prod_name + "-" + date + "-" + month + "-" + year;
     return name;
 }
 
@@ -35,20 +37,79 @@ app.get("/inventory", function (req, res) {
     }
 });
 
-app.post("/inventory", function (req, res) {
+app.get("/updateInventory", function (req, res) {
     try {
-        const savePath = path.join((electron.app || electron.remote.app).getPath('userData'), `/saveFiles/${nameGenerator(req.body.prod_name)}`);
-        const dir = path.join((electron.app || electron.remote.app).getPath('userData'), "saveFiles")
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, {
-                recursive: true
-            });
-        }
-        fs.writeFileSync(savePath, JSON.stringify(req.body));
-        res.redirect("/")
+        res.sendFile(__dirname + "/windows/updateInventory.html");
     } catch (error) {
         console.error(error);
     }
 });
 
+app.post("/updateInventory", function (req, res) {
+    try {
+        const savePath = (electron.app || electron.remote.app).getPath('userData') + `/saveFiles/${nameGenerator(req.body.prod_name)}.json`;
+        const dir = path.join((electron.app || electron.remote.app).getPath('userData'), "saveFiles");
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, {
+                recursive: true
+            });
+        }
+        // return
+        console.log(savePath);
+        let data = JSON.stringify(req.body);
+
+        fs.writeFileSync(savePath, data);
+        res.redirect("/");
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get("/billing", function (req, res) {
+    try {
+        res.sendFile(__dirname + "/windows/billing.html");
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get("/cloud", function (req, res) {
+    try {
+        res.sendFile(__dirname + "/windows/cloud.html");
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get("/expireProducts", function (req, res) {
+    try {
+        res.sendFile(__dirname + "/windows/expireProducts.html");
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get("/expireProductsList", function (req, res) {
+    try {
+        const dir = path.join((electron.app || electron.remote.app).getPath('userData'), "saveFiles");
+        const expDate = new Date(); 
+        expDate.setDate(expDate.getDate() + 30);
+        fs.readdir(dir, (err, files) => {
+            if (err) {
+                console.error(err);
+            }
+            let data = [];
+            files.forEach(file => {
+                const content = fs.readFileSync(dir + "/" + file);
+                prod_exp = new Date(JSON.parse(content).prod_exp)
+                if(prod_exp < expDate) {
+                    data.push(JSON.parse(content));
+                }                
+            });
+            res.send(data);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+})
 module.exports = app;
