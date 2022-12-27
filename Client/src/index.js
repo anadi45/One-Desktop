c4b435707fd76b7560303ff257d793d58ec30a19
anadi45 <ranjeetbaraik15@gmail.com>
2022-12-27 18:25:06 +0530

    Notifications Added

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,5 +1,5 @@
 const electron = require("electron");
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Notification  } = require('electron');
 const path = require('path');
 const server = require('./app');
 
@@ -31,10 +31,17 @@ const createWindow = () => {
   // console.log(userDataPath)
 };
 
+const NOTIFICATION_TITLE = 'Cloud Sync Update'
+const NOTIFICATION_BODY = 'Checkout Sync to Cloud Page to Upload Local Data to Cloud Database.'
+
+function showNotification () {
+  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+// app.on('ready', createWindow);
+app.whenReady().then(createWindow).then(showNotification)
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
